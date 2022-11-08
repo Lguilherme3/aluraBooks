c47d695f1d319ee3a498579fdc30d77c75f3f682
Guilherme <gui_3@outlook.com>
2022-11-08 18:55:43 -0300

    2° modularizando icones e opções header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import './App.css';
 import Logo from './componentes/Logo'
-import perfil from './imagens/perfil.svg'
-import sacola from './imagens/sacola.svg'
+import OpcoesHeader from './componentes/OpcoesHeader'
+import IconesHeader from './componentes/IconesHeader'
 
-const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
-const icones = [perfil, sacola]
+
+
+
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
        <Logo></Logo>
+       <OpcoesHeader></OpcoesHeader>
+       <IconesHeader></IconesHeader>
+
        
-       <ul className='opcoes'>
-        {textoOpcoes.map( (texto) => (
-          <li className='opcao'><p>{texto}</p></li>
-        ) )}
-       </ul>
-
-       <ul className='icones'>
-          { icones.map( (icone) => (
-            <li className='icone'><img src={icone}></img></li>
-          ) ) }
-
-       </ul>
       </header>
     </div>
   )
--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,6 +1,8 @@
 import './estilo.css'
 import logo from '../../imagens/logo.svg'
 
+
+
 function Logo() {
     return (
         <div className='logo'>
