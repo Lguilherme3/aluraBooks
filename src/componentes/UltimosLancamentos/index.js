aa64ae452a037e8400baddee47483ba537d3eb4e
Guilherme <gui_3@outlook.com>
2022-11-09 21:36:46 -0300

    6° finalizando o curso com props.

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,5 +1,7 @@
 import { livros } from './dadosUltimosLancamentos'
 import { Titulo } from '../Titulo'
+import CardRecomenda from '../CardRecomenda'
+import imagemLivro from '../../imagens/livro2.png'
 import styled from 'styled-components'
 
 
@@ -21,12 +23,20 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
     return (
         <UltimosLancamentosContainer>
-            <Titulo cor="#EB9B00">Últimos Lançamentos</Titulo>
+            <Titulo cor="#EB9B00" 
+            tamanhoFonte="36px" 
+            >Últimos Lançamentos</Titulo>
             <NovosLivrosContainer>
                 {livros.map( livro => (
                     <img src={livro.src}/>
                 ))}
             </NovosLivrosContainer>
+           <CardRecomenda
+           titulo="Talvez você se interesse por"
+           subtitulo="Angular 11"
+           descricao="Construindo uma aplicação com a plataforma Google."
+            img={imagemLivro}   
+           />
         </UltimosLancamentosContainer>
     )
 }
